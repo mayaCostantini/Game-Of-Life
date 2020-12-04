c193cc66d591e044537a5ccc258dd52121a18873
Maya Costantini <maya.cost@hotmail.fr>
2020-12-04 16:40:24 +0100

    Slight modification of gameOfLife.py

--- gameOfLife.py
+++ gameOfLife.py
@@ -19,33 +19,23 @@ class gameOfLife :
         neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
 
         rows = len(self.grid)
-        cols = len(self.grid[0])
+        columns = len(self.grid[0])
 
-        copy_board = [[self.grid[row][col] for col in range(cols)] for row in range(rows)]
+        newBoard = [[self.grid[row][col] for col in range(columns)] for row in range(rows)]
 
         for row in range(rows):
-            for col in range(cols):
-
+            for col in range(columns):
                 live_neighbors = 0
                 for neighbor in neighbors:
-
                     r = (row + neighbor[0])
                     c = (col + neighbor[1])
-
-                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                    if (r < rows and r >= 0) and (c < columns and c >= 0) and (newBoard[r][c] == 1):
                         live_neighbors += 1
-
-                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                if newBoard[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
                     self.grid[row][col] = 0
-                if copy_board[row][col] == 0 and live_neighbors == 3:
+                if newBoard[row][col] == 0 and live_neighbors == 3:
                     self.grid[row][col] = 1
 
         return self.grid
-
-
-    def launchIterations(self) : 
-
-        for _ in range(self.maxIter) : 
-            self.nextGeneration()
 
 
